80a2f373483f89bc5c12ccab9bc46a65033cd7e3
thefourhorsemen-4 <fourhorsemen987@gmail.com>
2019-03-19 16:10:12 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { ModalComponent } from './component/modal/modal.component';
+import { AboutComponent } from './component/about/about.component';
+import { ServiceComponent } from './component/service/service.component';
+import { GalleryComponent } from './component/gallery/gallery.component';
+import { ProjectComponent } from './component/project/project.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    ModalComponent,
+    AboutComponent,
+    ServiceComponent,
+    GalleryComponent,
+    ProjectComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
