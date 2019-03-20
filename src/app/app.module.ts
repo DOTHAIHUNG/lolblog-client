99d864de9b66bfa2bf37ee9685584504293f66e2
thefourhorsemen-4 <fourhorsemen987@gmail.com>
2019-03-20 17:12:50 +0700

    created routing for homepage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './component/homepage/navbar/navbar.component';
-import { BannerComponent } from './component/homepage/banner/banner.component';
-import { ModalComponent } from './component/homepage/modal/modal.component';
-import { AboutComponent } from './component/homepage/about/about.component';
-import { ServiceComponent } from './component/homepage/service/service.component';
-import { GalleryComponent } from './component/homepage/gallery/gallery.component';
-import { ProjectComponent } from './component/homepage/project/project.component';
-import { ContactComponent } from './component/homepage/contact/contact.component';
-import { FooterComponent } from './component/homepage/footer/footer.component';
-import { LoginComponent } from './component/login/login.component';
-import { HomepageComponent } from './component/homepage/homepage.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './component/homepage/navbar/navbar.component';
+import {BannerComponent} from './component/homepage/banner/banner.component';
+import {ModalComponent} from './component/homepage/modal/modal.component';
+import {AboutComponent} from './component/homepage/about/about.component';
+import {ServiceComponent} from './component/homepage/service/service.component';
+import {GalleryComponent} from './component/homepage/gallery/gallery.component';
+import {ProjectComponent} from './component/homepage/project/project.component';
+import {ContactComponent} from './component/homepage/contact/contact.component';
+import {FooterComponent} from './component/homepage/footer/footer.component';
+import {LoginComponent} from './component/login/login.component';
+import {HomepageComponent} from './component/homepage/homepage.component';
 
 @NgModule({
   declarations: [
@@ -37,4 +37,5 @@ import { HomepageComponent } from './component/homepage/homepage.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
